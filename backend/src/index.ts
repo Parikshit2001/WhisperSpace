862d63665fd65146aa1079ddb285cfee6181d482
Parikshit <parikshitgupta2001@gmail.com>
2024-11-06 21:27:34 +0530

    Add middlewares for cors, url-encoding and cookie parsing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import { app } from "./app";
+import dotenv from "dotenv";
+// Load environment variables
 
-const app = express();
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Server is Healthy and Running");
-});
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
