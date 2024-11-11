7ce80b98f4da6b02f8f48e8e6b0f6b42c8727d37
Parikshit <parikshitgupta2001@gmail.com>
2024-11-11 13:46:36 +0530

    last commit, wrapping up

--- backend/src/ws/index.ts
+++ backend/src/ws/index.ts
@@ -4,9 +4,12 @@ export function handleSocketConnections(io: Server) {
   io.on("connection", (socket) => {
     console.log("A user connected: ", socket.id);
 
-    socket.on("chat-message", (msg) => {
+    socket.on("receive-message", (msg, name, time) => {
+      console.log({ msg, name, time });
       socket.broadcast.emit("chat message", msg);
     });
+
+    socket.on("", () => {});
 
     socket.on("disconnect", () => {
       console.log("A user disconnected: ", socket.id);
